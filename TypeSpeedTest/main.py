1dc3186d9be06419dc475d8265ee97e9df9871aa
kactok <104381556+kactok@users.noreply.github.com>
2023-11-01 13:03:48 +0100

    Code changes

--- TypeSpeedTest/main.py
+++ TypeSpeedTest/main.py
@@ -7,24 +7,58 @@ from texts import data
 texts_dict = data
 text_num = 0
 character = 0
-
+last_correct_count = 0
+timer_1 = None
+correct_char_sum = 0
+correct_char = 0
+cpm_count = 0
+wpm_count = 0
 
 def change_text():
-    global text_num
+    global text_num, last_correct_count, correct_char_sum, correct_char, wpm_count, cpm_count
     if len(entry_text.get("1.0", END)) == len(texts_dict[text_num]) + 1:
         text_num += 1
         display_text.config(text=texts_dict[text_num])
+        correct_char_sum += correct_char
         entry_text.delete('1.0', END)
-    print(entry_text.get("end-2c"))
-    print(texts_dict[text_num][int(float(entry_text.index("end-2c"))*10 % 10)])
-    # if entry_text.get('1.0', END) != texts_dict[text_num][:len(entry_text.get('1.0', END)) - 1]:
-    if texts_dict[text_num][int(float(entry_text.index("end-2c"))*10 % 10)] != entry_text.get("end-2c"):
-        entry_text.tag_config("start", foreground="red")
-        entry_text.tag_add("start", entry_text.index("end-2c"))
+    char_index = str(entry_text.index("end-2c")).replace("1.", "")
+    if texts_dict[text_num][int(char_index)] != entry_text.get("end-2c"):
+        entry_text.tag_config(char_index, foreground="red")
+        entry_text.tag_add(char_index, entry_text.index("end-2c"))
     else:
-        print("Good")
-        entry_text.tag_delete("start")
+        entry_text.tag_delete(char_index)
+        correct_char = correct_char_sum + len(entry_text.get("1.0", END)) - 1
+        if correct_char > last_correct_count:
+            wpm_count = (correct_char / 5)
+            cpm_count = correct_char
+            wpm.config(text=f"WPM: {wpm_count}")
+            cpm.config(text=f"CPM: {cpm_count}")
+            last_correct_count = len(entry_text.get("1.0", END)) - 1
     window.after(100, change_text)
+
+
+def start():
+    global wpm_count, cpm_count
+    wpm_count = 0
+    cpm_count = 0
+    wpm.config(text=f"WPM: {wpm_count}")
+    cpm.config(text=f"CPM: {cpm_count}")
+    entry_text.config(state="normal")
+    entry_text.delete('1.0', END)
+    entry_text.focus_set()
+    start_timer(60)
+
+
+def start_timer(value):
+    global timer_1
+    timer.config(text=f"Time left: {value}")
+    if value > 0:
+        timer_1 = window.after(1000, start_timer, value - 1)
+        str_button.config(state="disabled")
+    else:
+        window.after_cancel(timer_1)
+        entry_text.config(state="disabled")
+        str_button.config(state="normal")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -32,8 +66,7 @@ def change_text():
 # Init window
 window = tb.Window(themename='darkly')
 window.title('Speed Typing')
-window.geometry('600x250+650+300')
-
+window.geometry('600x320+650+300')
 # Display text
 
 display_text = tkinter.Label(text=f"Text to type:\n{texts_dict[text_num]}",
@@ -44,19 +77,63 @@ display_text = tkinter.Label(text=f"Text to type:\n{texts_dict[text_num]}",
 
 display_text.grid(column=1,
                   row=1,
+                  columnspan=2,
                   padx=20,
-                  pady=40)
+                  pady=20)
+
+# Add timer
+timer = tkinter.Label(text="Time left: 60",
+                      font=('Helvetica', 12),
+                      anchor="center", )
+timer.grid(column=2,
+           row=4,
+           padx=20,
+           pady=20)
+
+# Add labels words/min and chars/min
+
+wpm = tkinter.Label(text=f"WPM: {wpm_count}",
+                    font=('Helvetica', 12),
+                    anchor="center", )
+wpm.grid(column=1,
+         row=2,
+         padx=20,
+         pady=20)
+
+cpm = tkinter.Label(text=f"CPM: {cpm_count}",
+                    font=('Helvetica', 12),
+                    anchor="center", )
+cpm.grid(column=2,
+         row=2,
+         padx=20,
+         pady=20)
 
 # Add Text entry
 entry_text = tkinter.Text(highlightbackground='#409700')
 entry_text.config(fg='#40b400',
                   font=('Helvetica', 12),
                   width=50,
-                  height=3)
-entry_text.focus_set()
+                  height=3,
+                  insertwidth=3,
+                  state="disabled")
 entry_text.grid(column=1,
-                row=2,
+                row=3,
                 padx=70,
+                columnspan=2
                 )
+
+# Buttons
+new_style = tb.Style()
+new_style.configure('success.TButton',
+                    font=('Helvetica', 12),
+                    width=15)
+
+str_button = tb.Button(text='Start', style='success.TButton', command=start)
+str_button.grid(column=1,
+                row=4,
+                padx=38,
+                pady=10
+                )
+
 change_text()
 window.mainloop()
---
+++ TypeSpeedTest/texts.py
@@ -0,0 +1,13 @@
+data = {
+    0: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.",
+    1: "Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack.",
+    2: "Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex.",
+    3: "Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! 'Now fax quiz Jack!' my brave ghost pled. Five quacking zephyrs jolt my wax bed.",
+    4: "Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box.",
+    5: "The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog.",
+    6: "Watch 'Jeopardy!', Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just flocked up to quiz and vex him.",
+    7: "My faxed joke won a pager in the cable TV quiz show. Amazingly few discotheques provide jukeboxes. My girl wove six dozen plaid jackets before she quit.",
+    8: "Big July earthquakes confound zany experimental vow. Foxy parsons quiz and cajole the lovably dim wiki-girl. Have a pick: twenty six letters - no forcing a jumbled quiz!",
+    9: "Sixty zippers were quickly picked from the woven jute bag. A quick movement of the enemy will jeopardize six gunboats. All questions asked by five watch experts amazed the judge.",
+    10: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs.",
+}
